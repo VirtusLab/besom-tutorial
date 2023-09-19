da9a898e4e66993294c47e52db4347d2616e7c55
Paweł Prażak <pprazak@virtuslab.com>
2023-09-19 17:57:42 +0200

    Work in progress

--- infra/index.ts
+++ infra/index.ts
@@ -27,11 +27,9 @@ let bucketPublicAccessBlock = new aws.s3.BucketPublicAccessBlock(`${bucketName}-
     restrictPublicBuckets: false // Do not restrict public bucket policies for this bucket
 });
 
-// const tableName = "pulumi-catpost-table";
-const tableName = "CatPostTable";
+const tableName = "pulumi-catpost-table";
 
-/*
-const catPostTable = new aws.dynamodb.Table("cat-post-table", {
+const catPostTable = new aws.dynamodb.Table(tableName, {
   name: tableName,
   attributes: [
     { name: "PartitionKey", type: "S" },
@@ -42,47 +40,6 @@ const catPostTable = new aws.dynamodb.Table("cat-post-table", {
   readCapacity: 5,
   writeCapacity: 5,
 });
-
-const entry1 = new aws.dynamodb.TableItem("entry1", {
-  tableName: catPostTable.name,
-  hashKey: catPostTable.hashKey,
-  item: `{
-    PartitionKey: { S: "ALL_ENTRIES" },
-    timestamp: { S: "2023-09-16T12:00:00Z" },
-    entryId: { S: "entry1" },
-    userName: { S: "User1" },
-    comment: { S: "Lovely cat!" },
-    catPictureURL: { S: "https://cdn2.thecatapi.com/images/57r.jpg" }
-  }`
-});
-
-const entry2 = new aws.dynamodb.TableItem("entry2", {
-  tableName: catPostTable.name,
-  hashKey: catPostTable.hashKey,
-  item: `{
-    PartitionKey: { S: "ALL_ENTRIES" },
-    timestamp: { S: "2023-09-16T12:05:00Z" },
-    entryId: { S: "entry2" },
-    userName: { S: "User2" },
-    comment: { S: "Such a cute kitty!" },
-    catPictureURL: { S: "https://cdn2.thecatapi.com/images/buf.jpg" }
-  }`
-});
-
-const entry3 = new aws.dynamodb.TableItem("entry3", {
-  tableName: catPostTable.name,
-  hashKey: catPostTable.hashKey,
-  item: `{
-    PartitionKey: { S: "ALL_ENTRIES" },
-    timestamp: { S: "2023-09-16T12:10:00Z" },
-    entryId: { S: "entry3" },
-    userName: { S: "User3" },
-    comment: { S: "Adorable!" },
-    catPictureURL: { S: "https://cdn2.thecatapi.com/images/LzVDEMYIv.jpg" }
-  }`
-});
-
-*/
 
 const stageName = "default"
 const feedName = "pulumi-render-feed"
