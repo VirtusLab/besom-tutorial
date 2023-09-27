165e01cc77a3b0b9bb84330b87f92e730b4fd70f
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2023-09-27 15:06:18 +0200

    bump to 0.1.0 yay

--- besom/project.scala
+++ besom/project.scala
@@ -1,4 +1,4 @@
 //> using scala "3.3.0"
-//> using plugin "org.virtuslab::besom-compiler-plugin:0.0.2-SNAPSHOT"
-//> using lib "org.virtuslab::besom-core:0.0.2-SNAPSHOT"
-//> using lib "org.virtuslab::besom-aws:6.2.0-core.0.0.2-SNAPSHOT"
+//> using plugin "org.virtuslab::besom-compiler-plugin:0.1.0"
+//> using lib "org.virtuslab::besom-core:0.1.0"
+//> using lib "org.virtuslab::besom-aws:6.2.1-core.0.1.0"
