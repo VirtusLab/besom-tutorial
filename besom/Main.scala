7bd886582109017675b0c1c2415b8edcd69b2aa9
Paweł Prażak <pprazak@virtuslab.com>
2024-04-19 19:04:26 +0100

    Update besom to version 0.3.1

--- besom/Main.scala
+++ besom/Main.scala
@@ -1,7 +1,5 @@
 import besom.*
 
 @main def main: Unit = Pulumi.run {
-  for 
-    _ <- log.warn("Nothing's here yet, it's waiting for you to write some code!")
-  yield Pulumi.exports()
+  Stack(log.warn("Nothing's here yet, it's waiting for you to write some code!"))
 }
